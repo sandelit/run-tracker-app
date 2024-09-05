55b1c547198d45c8b4f68c85a9850bb99cfb1076
sandelit <thomas.sandelin@gmail.com>
2024-09-06 00:22:46 +0300

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.runtracker.app',
+  appName: 'run-tracker-app',
+  webDir: 'dist/run-tracker-app/browser'
+};
+
+export default config;
