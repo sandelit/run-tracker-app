65fe2fb682754db17b74debd999dd0d35f313052
sandelit <thomas.sandelin@gmail.com>
2024-09-09 00:27:47 +0300

    map initialized and marker at current position

--- src/app/leaflet-map/leaflet-map.component.ts
+++ src/app/leaflet-map/leaflet-map.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import * as L from "leaflet";
+import { Geolocation } from "@capacitor/geolocation";
 
 @Component({
   selector: "app-leaflet-map",
   templateUrl: "./leaflet-map.component.html",
   styleUrls: ["./leaflet-map.component.scss"],
-  standalone: true
+  standalone: true,
 })
 export class LeafletMapComponent implements OnInit {
   constructor() {}
@@ -14,12 +15,18 @@ export class LeafletMapComponent implements OnInit {
     this.initMap();
   }
 
-  private initMap(): void {
-    const map = L.map("map").setView([51.505, -0.09], 13);
-
+  private async initMap(): Promise<void> {
+    const position = await this.getCurrentPosition();
+    const { latitude, longitude } = position.coords;
+    const map = L.map("map").setView([latitude, longitude], 13);
+    const marker = L.marker([latitude, longitude]).addTo(map);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
   }
+
+  private getCurrentPosition = async () => {
+    return await Geolocation.getCurrentPosition();
+  };
 }
